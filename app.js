e72df3e944386e8ea4fb267f8fb6d554e3518ab1
Sedenu <aloayecharles@gmail.com>
2025-01-27 19:23:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) =>{
+
+  res.send('Hello World');
+
+  res.end();
+});
+
 app.use("/users", UsersRoutes);
 app.use("/wallets", WalletsRoutes);
 app.use("/", HomeRoutes);
